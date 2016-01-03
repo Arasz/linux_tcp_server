d047ef9c8619e6e9f9bf9db343adbc7b7338c019
AraszDev <rafal.araszkiewicz@student.put.poznan.pl>
2016-01-03 13:07:38 +0100

    #0.0.1 All server methods are written.

--- inc/tcpserver.h
+++ inc/tcpserver.h
@@ -10,10 +10,12 @@
 
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <sys/unistd.h>
 #include <netinet/in.h>
 #include <iostream>
 #include <string>
 #include "tcp_server_exception.h"
+#include <functional>
 
 namespace mrobot
 {
@@ -23,16 +25,22 @@ class tcp_server
 public:
 	tcp_server();
 	virtual ~tcp_server();
-
+	void send(char* buffer, int length);
 
 private:
 	void create_socket();
 	void bind_socket(int port);
 	void listen_for_connection();
 	void accept_connection();
+	int read_data();
 
-	int _socket_file_descriptor;
-	int _port;
+	std::function<void()> on_data_ready;
+	const size_t _buffer_size = 255;
+	char* _buffer[255]; /// communication buffer
+	int _socket_file_descriptor = 0; /// server socket file descriptor
+	int _port = 22222; /// port on which server listens
+	sockaddr_in _client_addres; /// connected client address
+	int _client_socket_file_descriptor = 0; /// socket used for communication which connected client
 };
 
 } /* namespace mrobot */
--- src/tcpserver.cpp
+++ src/tcpserver.cpp
@@ -22,7 +22,7 @@ tcp_server::~tcp_server()
 }
 
 /**
- * @brief Creates new, nonblocking TCP/IP server socket
+ * @brief Create new TCP/IP server socket
  *
  * @throws tcp_server_exception
  */
@@ -30,14 +30,10 @@ void tcp_server::create_socket()
 {
 	// create ipv4 no blocking socket, with full duplex byte stream communication
 	// protocol is chosen by OS
-	_socket_file_descriptor = socket(PF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
+	_socket_file_descriptor = socket(PF_INET, SOCK_STREAM, 0); // | SOCK_NONBLOCK, 0);
 
 	if(_socket_file_descriptor<0)
-	{
-		std::string error_msg{"Error when opening socket.\n"};
-		std::cerr<<error_msg;
-		throw tcp_server_exception{error_msg};
-	}
+		throw tcp_server_exception{"Error when opening socket.\n"};
 }
 
 /**
@@ -49,21 +45,20 @@ void tcp_server::bind_socket(int port)
 {
 	sockaddr_in socket_addres;
 
-	// Set address family for ipv4
+	// set all values in buffer to a zero
+	//bzero(reinterpret_cast<char*>(&socket_addres), sizeof(socket_addres));
+
+	// set address family for ipv4
 	socket_addres.sin_family = AF_INET;
-	// Save port number in network byte order
+	// save port number in network byte order
 	socket_addres.sin_port = htons(port);
-	// Set ip address of the host to the address of machine on which server is running
+	// set ip address of the host to the address of machine on which server is running
 	socket_addres.sin_addr.s_addr = INADDR_ANY;
 
 	int bind_ret_val = bind(_socket_file_descriptor, reinterpret_cast<sockaddr*>(&socket_addres), sizeof(socket_addres));
 
 	if(bind_ret_val < 0 )
-	{
-		std::string error_msg = "Binding socket to address failed.";
-		std::cerr<<error_msg;
-		throw tcp_server_exception{error_msg};
-	}
+		throw tcp_server_exception{"Binding socket to address failed."};
 
 }
 
@@ -78,11 +73,35 @@ void tcp_server::listen_for_connection()
 	listen(_socket_file_descriptor, 5);
 }
 
+void tcp_server::send(char* buffer, int length)
+{
+}
+
 /**
  * @brief Accept connection from client and creates new socket for communication
+ * @throws tcp_server_exception
  */
 void tcp_server::accept_connection()
 {
+	unsigned int client_address_size = sizeof(_client_addres);
+	_client_socket_file_descriptor = accept(_socket_file_descriptor, reinterpret_cast<sockaddr*>(&_client_addres), &client_address_size);
+
+	if(_client_socket_file_descriptor < 0)
+		throw tcp_server_exception("Error when accepting connection.\n");
+
+}
+
+/**
+ * @brief Read data from client and write to class buffer
+ * @return read bytes count
+ * @throws tcp_server_exception
+ */
+int tcp_server::read_data()
+{
+	int bytes_count = read(_client_socket_file_descriptor, _buffer, _buffer_size);
+	if(bytes_count < 0)
+		throw tcp_server_exception{"Error reading data from socket"};
+	return bytes_count;
 }
 
 } /* namespace mrobot */
